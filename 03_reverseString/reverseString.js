9de6e2da6b1c02dea7450d5904fbaee589b41a41
ernmin <42770608+ernmin@users.noreply.github.com>
2024-06-08 17:08:44 +0800

    Attempt Reverse String Exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let holdStr = str;
+    for (let i = str.length; i > 0; i--){
+        holdStr[str.length - i] += str[i];
+    }
+    return holdStr;
 };
 
 // Do not edit below this line
