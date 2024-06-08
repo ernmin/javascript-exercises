89ec10ec6a0e422be13eac700a0a06e2eafe5169
ernmin <42770608+ernmin@users.noreply.github.com>
2024-06-08 16:48:55 +0800

    Complete Repeat String Exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, numOfTimes) {
+        numOfTimes = parseInt(numOfTimes);
+        if (Math.sign(numOfTimes) < 0){
+            return 'ERROR';
+        }
+        let finalStr = "";
+        for (let i = 0; i < numOfTimes; i++){
+            finalStr += str; 
+        }
+        return finalStr;
 };
 
 // Do not edit below this line
