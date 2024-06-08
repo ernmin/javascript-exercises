71bad95f088b94fcbd2ba960a2d2b8a09bb33b10
ernmin <42770608+ernmin@users.noreply.github.com>
2024-06-09 02:21:29 +0800

    Attempt removeFromArray Exercise Fail test 'removes multiple of the same value'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr1, ...passInArg) {
+    // const arg = Array.from(passInArg);
+    for (const arg of passInArg){
+        //let j = 1; j < arg.length - 1; j++){
+        for (let i = 0; i < arr1.length; i++){
+            if (arr1[i] === arg){
+                arr1.splice(i, 1);
+            }
+        }
+    }
+    return arr1;
 };
+//Unsure why the test case 'removes multiple of the same value' fails.
 
 // Do not edit below this line
 module.exports = removeFromArray;
