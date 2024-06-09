36aa45713e3fe8b96e5d75caf1cf08ca221744ef
ernmin <42770608+ernmin@users.noreply.github.com>
2024-06-09 10:11:43 +0800

    Attempt stratagy with push() Instead the push() executes double because of the outer for loop.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,21 @@
 const removeFromArray = function(arr1, ...passInArg) {
     // const arg = Array.from(passInArg);
+    const newArr = [];
     for (const arg of passInArg){
         //let j = 1; j < arg.length - 1; j++){
         for (let i = 0; i < arr1.length; i++){
             if (arr1[i] === arg){
-                arr1.splice(i, 1);
+                continue;
+            }
+            else {
+                newArr.push(arr1[i]);
             }
         }
     }
-    return arr1;
+    return newArr;
 };
-//Unsure why the test case 'removes multiple of the same value' fails.
+// Unsure why the test case 'removes multiple of the same value' fails.
+// Use a new array but now the push() functions does double work so it is not correct
 
 // Do not edit below this line
 module.exports = removeFromArray;
