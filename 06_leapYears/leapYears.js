accfc0da827310660a364005fd52815afd2fb1f1
ernmin <42770608+ernmin@users.noreply.github.com>
2024-06-10 21:33:38 +0800

    Complete leapYears Exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+// take in a number
+// check if a year is divisible by 4 and not divisible by 100, this is a leap year
+if (year % 4 === 0 && year % 100 !== 0){
+    return true;
+}
+else if (year % 400 === 0){
+    return true;
+}
+else{
+    return false;
+}
+// check if a year is division by 400, this is a leap year.
 };
 
 // Do not edit below this line
