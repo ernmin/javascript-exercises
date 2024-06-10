b297cb409c65cd9dac3b8e005b0c7422f4dfdfd5
ernmin <42770608+ernmin@users.noreply.github.com>
2024-06-10 23:38:14 +0800

    Complete tempConversion Exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  let celcius;
+  celcius = (tempInFahrenheit - 32) * (5 / 9); 
+  celcius = Number(celcius.toFixed(1));
+  return celcius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelcius) {
+  let fahrenheit;
+  fahrenheit = (tempInCelcius * (9 / 5)) + 32;
+  fahrenheit = Number(fahrenheit.toFixed(1));
+  return fahrenheit
 };
 
 // Do not edit below this line
