ed8783c9df0f3cce0dffe5159ecb95a994475bcb
ernmin <42770608+ernmin@users.noreply.github.com>
2024-06-12 02:56:47 +0800

    Complete Fibonacci Exercise Solve problem recursively using base case 0 and base case 1 Learn how to draw a resursion tree

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(elementInSeries) {
+    if (typeof(elementInSeries) != Number){    
+        elementInSeries = Number(elementInSeries);
+    }
+    if (elementInSeries < 0){
+        return "OOPS"
+    }
+    if (elementInSeries === 0){
+        return 0;
+    }
+    else if (elementInSeries === 1){
+        return 1;
+    }
+    else {
+        return fibonacci(elementInSeries - 1) + fibonacci(elementInSeries - 2);
+    }
 };
 
 // Do not edit below this line
