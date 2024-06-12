fa29adec34a96aa474a29e3e22cd8f34165a4c1a
ernmin <42770608+ernmin@users.noreply.github.com>
2024-06-13 00:02:57 +0800

    Complete getTheTitles Exercise Create empty array Loop through each book in the array of objects Retrieve the title using the . operator, book.title Use push method to add element to the empty array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let arrayOfTitles = [];
+    let storeTitle;
+    for (let book of books){
+        storeTitle = book.title;
+        arrayOfTitles.push(storeTitle);
+    }
+    return arrayOfTitles;
 };
 
 // Do not edit below this line
