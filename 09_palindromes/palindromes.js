8f680feafd4851160cb088da45e41479888be723
ernmin <42770608+ernmin@users.noreply.github.com>
2024-06-12 23:43:09 +0800

    Complete Palindromes Exercise Remove punctuation using str.replace method Remove whitespace using str.replace method

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // write two base cases
+    str = str.toLowerCase();
+    str = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+    str = str.replace(' ', '')
+    if (str.length === 1){
+        return true;
+    }
+    else if(str.length === 2 && str.at(0) === str.at(1)){
+        return true;
+    }
+    // check first and last letters are not the same
+    else if(str.at(0) !== str.at(str.length - 1))
+        return false;
+    // else pass next string 
+    else {
+        let newStr = str;
+        newStr = newStr.slice(1);
+        newStr = newStr.substring(0, newStr.length - 1)
+        return palindromes(newStr);
+    }
 
 };
 
