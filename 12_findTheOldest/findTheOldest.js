fee8180d34a1eb5366ef78c0044ac226c8dd9396
ernmin <42770608+ernmin@users.noreply.github.com>
2024-07-01 12:36:27 +0800

    Complete exercise 12 At first, only the name of the person was returned. However, the test should return the person object.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let highest = 0;
+    let currentPerson = null;
+    arr.forEach(function(person){
+        if ((person.yearOfDeath - person.yearOfBirth) > highest){
+            currentPerson = person
+            highest = person.yearOfDeath - person.yearOfBirth;
+        }
+    });
+    return currentPerson;
+    
 
+    //try using reduce, only return a value if it is greater than the previous greatest, else don't return
 };
 
 // Do not edit below this line
