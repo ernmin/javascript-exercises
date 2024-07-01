ba91a0170a3e0477cb6bfc5766f39e25953ba07a
ernmin <42770608+ernmin@users.noreply.github.com>
2024-07-01 13:10:41 +0800

    Complete exercise 12 if person has no death date Use 'person.hasOwnProperty("yearOfDeath")' Use todayDate.getFullYear() method Update current person and highest age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,13 +1,23 @@
 const findTheOldest = function(arr) {
     let highest = 0;
     let currentPerson = null;
+    const todayDate = new Date();
     arr.forEach(function(person){
-        if ((person.yearOfDeath - person.yearOfBirth) > highest){
-            currentPerson = person
+        let hasyearOfDeath = person.hasOwnProperty("yearOfDeath");
+        if (!hasyearOfDeath){
+            let currentAge = todayDate.getFullYear() - person.yearOfBirth;
+            if (currentAge > highest){
+                currentPerson = person;
+                highest = currentAge;
+            }
+        }
+        else if ((person.yearOfDeath - person.yearOfBirth) > highest){
+            currentPerson = person;
             highest = person.yearOfDeath - person.yearOfBirth;
         }
     });
     return currentPerson;
+                
     
 
     //try using reduce, only return a value if it is greater than the previous greatest, else don't return
